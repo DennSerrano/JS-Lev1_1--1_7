b489937e2ab8f0b81a40467c59d340819800deaf
Dennserrano <dennserrano@gmail.com>
2021-10-25 13:21:00 +0200

    lev1_1-1_7

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+let helloWorld = "Hallo World"
+console.log(helloWorld)
+console.log(helloWorld)
+
+let fullName = "Jan Schmidt" 
+console.log(fullName)
+
+let = camelCase = "camelCase"
+console.log(camelCase)
+
+let pI = 3.14 
+console.log(pI)
+
+const burjDubai = "828m"
+console.log(burjDubai)
+
+const eifelTower = "324m"
+console.log(eifelTower)
+
+let kursStatus = true
+console.log(kursStatus) 
+
+
